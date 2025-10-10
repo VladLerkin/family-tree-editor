70df4ab8f05da76d7bd5f20328576d5eadb5fd34
yav <vladlerkin@gmail.com>
2025-10-10 14:12:12 +0400

    REL-024: Fix GitHub CI/CD

--- src/main/java/com/pedigree/ui/AboutDialog.java
+++ src/main/java/com/pedigree/ui/AboutDialog.java
@@ -20,7 +20,7 @@ import java.net.URISyntaxException;
 public class AboutDialog {
 
     private static final String APP_NAME = "Family Tree Editor";
-    private static final String APP_VERSION = "1.2.0";
+    private static final String APP_VERSION = "v1.2.4";
     private static final String AUTHOR_EMAIL = "domfindus@gmail.com";
 
     public static void show() {
