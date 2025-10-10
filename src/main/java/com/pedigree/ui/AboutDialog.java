dc55db0e77f3ad6e53cde8a4b8b8deb2d247bd71
yav <vladlerkin@gmail.com>
2025-10-10 20:35:42 +0400

    Release version 1.2.19

--- src/main/java/com/pedigree/ui/AboutDialog.java
+++ src/main/java/com/pedigree/ui/AboutDialog.java
@@ -20,7 +20,7 @@ import java.net.URISyntaxException;
 public class AboutDialog {
 
     private static final String APP_NAME = "Family Tree Editor";
-    private static final String APP_VERSION = "v1.2.18";
+    private static final String APP_VERSION = "v1.2.19";
     private static final String AUTHOR_EMAIL = "domfindus@gmail.com";
 
     public static void show() {
