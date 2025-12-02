9e632287cd43c7f1902fbc4f8b1529b7f122a1f3
yav <vladlerkin@gmail.com>
2025-12-02 22:30:46 +0400

    Remove hardcoded JAVA_HOME from Config.xcconfig and bump version to v1.3.10

--- app-desktop/build.gradle.kts
+++ app-desktop/build.gradle.kts
@@ -34,7 +34,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "FamilyTreeEditor"
-            packageVersion = "1.3.9"
+            packageVersion = "1.3.10"
             
             description = "Family Tree Editor Application"
             copyright = "© 2024 Family Tree. All rights reserved."
