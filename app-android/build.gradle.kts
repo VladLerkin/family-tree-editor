50fab3cee549fa0ed3ad158f13287413a206b0dd
Vladislav Iarygin <vlad@Vladislavs-MacBook-Pro.local>
2026-01-05 20:03:17 +0400

    Fix Android update issue by centralizing versionCode and bump to v1.4.5

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.family.tree.android"
         minSdk = (project.findProperty("android.minSdk") as String).toInt()
         targetSdk = (project.findProperty("android.targetSdk") as String).toInt()
-        versionCode = 10
+        versionCode = (project.findProperty("app.versionCode") as String).toInt()
         versionName = project.version.toString()
         
         // Support multiple architectures
